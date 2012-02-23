23b8e23c083d767cc1787c40e63f3da5647944e1
Satish Irrinki <sgirrinki@yahoo.com>
2012-02-23 12:31:09 -0500

    Added files for the branch Release_3.1.0

--- Product/DBScripts/asyncmsgs/asyncmsgsdb.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `asyncmsgrepo`;
-
-CREATE TABLE asyncmsgs.asyncmsgrepo (
-	MessageId VARCHAR(100) NOT NULL,
-	CreationTime DATETIME NOT NULL,
-	ServiceName VARCHAR(100),
-	MsgData LONGBLOB,
-	PRIMARY KEY (MessageId)
-);
--- Product/DBScripts/nhincdb/toOracle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ksh
-
-for x in `ls -1 *.cfg.xml`
-do
-	echo $x
-	sed -f toOracle.sed $x > $x.new
-	mv $x.new $x
-done
